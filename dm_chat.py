cbe0a0637e832e2b503a0d2cf08580564f5cf564
icumdubstep <icumdubstep@gmail.com>
2013-07-14 15:30:07 -0500

    ANSI colors work, xterm colors don't. Surprise surprise.

--- dm_chat.py
+++ dm_chat.py
@@ -8,7 +8,7 @@ import dm_global
 class ChatChannel: # Class for a specific channel for the chat.
 	def __init__(self, close_on_vacant=True, name="System"):
 		self.name = name
-		self.color = "\033[38;5;1m"
+		self.color = "\x1b[31;1m"
 		self.readonly = False
 		self.topic = "Topic not set"
 		self.connected_players = []
@@ -35,72 +35,61 @@ class ChatChannel: # Class for a specific channel for the chat.
 			player.send_message("Not connected to {0}".format(self.name))
 		return False
 	def get_display_name(self):
-		return self.color + self.name + "\033[38;5;15m"
+		return self.color + self.name + "\x1b[37;1m"
 class ChatManager:
 	def __init__(self):
-		self.channels = [ChatChannel(False), ChatChannel(False, "default")]
-		self.channels[0].readonly = True
+		self.channels = {'System': ChatChannel(False), 'default': ChatChannel(False, "default")}
+		self.channels['System'].readonly = True
 	def add_channel(self, channel_name):
 		if ' ' in channel_name:
 			return "Channel name invalid"
-		for channel in self.channels:
-			if channel.name == channel_name:
-				return "Channel already open"
-		self.channels.append( ChatChannel( True, channel_name ) )
+		elif channel_name in self.channels:
+			return "Channel already open"
+		else:
+			self.channels[channel_name] = ChatChannel( True, channel_name )
 	#This is different from handle_message in that it a) changes the channel topic and b) takes the whole command as input for msg
-	def display_topic(self, player, target_channel):
-		channel_found = False
-		for channel in self.channels:
-			if channel.name == target_channel:
-				
-				channel_found = True
-				
-		if not channel_found:
+	def display_topic(self, player, channel):
+		try:
+			player.send_message("Topic for {0}: {1}".format((channel, self.channels[channel].topic)))
+		except KeyError as e:
 			player.send_message("Channel {0} not found".format(target_channel))
-	def change_topic(self, player, target_channel, topic):
-		channel_found = False
-		for channel in self.channels:
-			if channel.name == target_channel:
-				channel.topic = topic
-				channel.broadcast("*%s has changed the topic to \"%s\"" % (player.name, msg[msg.index(' ') + 1:]))
-				player.last_channel = target_channel
-				channel_found = True
-		if not channel_found:
+	def change_topic(self, player, channel, topic):
+		if channel in self.channels:
+			self.channels[channel].topic = topic
+			self.channels[channel].broadcast("*%s has changed the topic to \"%s\"" % (player.name, msg[msg.index(' ') + 1:]))
+			player.last_channel = channel
+		else:
 			player.send_message("Channel {0} not found".format(target_channel))
 	def handle_message(self, msg, player, target_channels=[], me=False):
-		missing_channels = target_channels
-		for channel in self.channels:
-			if channel.name in target_channels:
-				if channel.readonly and not player.admin:
+		for channel in target_channels:
+			if channel in self.channels:
+				if self.channels[channel].readonly and not player.admin:
 					player.send_message("You are not allowed to send messages to this channel.")
 				else: 
 					if me:
-						channel.broadcast("*%s %s" % (player.name, msg))
+						self.channels[channel].broadcast("*%s %s" % (player.name, msg))
 					else:
-						channel.broadcast("%s : %s" % (player.name, msg))
-					player.last_channel = channel.name
-				missing_channels.remove(channel.name)
-		for channel in missing_channels:
-			return "Channel {0} not found".format(channel)
-	def add_player_to_channel(self, player, channel_name="System"):
-		for channel in self.channels:
-			if channel.name == channel_name:
-				channel.add_player(player)
-				return
-		player.send_message("Channel %s not found." % channel_name)
-	def remove_player_from_channel(self, player, channel_name="System"):
-		for channel in self.channels:
-			if channel.name == channel_name:
-				if channel.remove_player(player):
-					self.channels.remove(channel)
-					dm_global.broadcast("Channel %s is vacant, and now will be closed." % channel.name)
-				return
-		player.send_message("Channel %s not found." % channel_name)
+						self.channels[channel].broadcast("%s : %s" % (player.name, msg))
+					player.last_channel = channel
+			else:
+				return "Channel {0} not found".format(channel)
+	def add_player_to_channel(self, player, channel="System"):
+		if channel in self.channels:
+			self.channels[channel].add_player(player)
+		else:
+			player.send_message("Channel %s not found." % channel)
+	def remove_player_from_channel(self, player, channel="System"):
+		if channel in self.channels:
+			if channel.remove_player(player):
+				del self.channels[channel]
+				dm_global.broadcast("Channel %s is vacant, and now will be closed." % channel)
+		else:
+			player.send_message("Channel %s not found." % channel)
 	def get_channels(self):
 		s = "List of all channels:"
 		for channel in self.channels:
 			s += "" + channel.name
 		return s
 	def remove_player_from_all_channels(self, player):
-		for channel in self.channels:
+		for id, channel in self.channels.iteritems():
 			channel.remove_player(player)
